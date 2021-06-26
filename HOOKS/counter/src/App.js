69dd26419216a2c6fd7a04609e87b70c4be9e282
robust-star <raj1999gupta@gmail.com>
2021-06-26 16:27:05 +0530

    objects

--- HOOKS/counter/src/App.js
+++ HOOKS/counter/src/App.js
@@ -1,13 +1,15 @@
 import ClassCounter from './components/ClassCounter';
 import './App.css';
 import HookCounte from './components/HookCounte';
-
+import HookCounter2 from './components/HookCounter2';
+import HookCounter3 from './components/HookCounter3';
 function App() {
   return (
     <div className="App">
       {/* <ClassCounter></ClassCounter> */}
-      <HookCounte></HookCounte>
-
+      {/* <HookCounte></HookCounte> */}
+      {/* <HookCounter2></HookCounter2> */}
+      <HookCounter3></HookCounter3>
     </div>
   );
 }
